17b92a13362a4e8577a5f3fdc64e104fa9e64ca8
Abraham Brian Diaz Condori <20201323D@utea.edu.pe>
2024-04-10 11:03:13 -0500

    aumento de clases Empleado DAO por error

--- Empleado.cs
+++ Empleado.cs
@@ -16,21 +16,4 @@ namespace ExamenII
             public double SueldoNeto { get; set; }
         }
 
-    using System.Collections.Generic;
-
-    public class EmpleadoDAO
-        {
-            private List<Empleado> empleados = new List<Empleado>();
-
-            public void AgregarEmpleado(Empleado empleado)
-            {
-                empleados.Add(empleado);
-            }
-
-            public List<Empleado> ObtenerEmpleados()
-            {
-                return empleados;
-            }
-        }
-
 }
---
+++ EmpleadoDAO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenII
+{
+    using System.Collections.Generic;
+
+    public class EmpleadoDAO
+    {
+        private List<Empleado> empleados = new List<Empleado>();
+
+        public void AgregarEmpleado(Empleado empleado)
+        {
+            empleados.Add(empleado);
+        }
+
+        public List<Empleado> ObtenerEmpleados()
+        {
+            return empleados;
+        }
+    }
+}
