16b33ce0022f718c6218c166450cc19b600d7576
Abraham Brian Diaz Condori <20201323D@utea.edu.pe>
2024-04-10 11:08:28 -0500

    llamado de clases

--- Form1.cs
+++ Form1.cs
@@ -12,22 +12,54 @@ using System.Windows.Forms;
     {
     public partial class Form1 : Form
         {
-            public Form1()
+        private EmpleadoDAO empleadoDAO = new EmpleadoDAO();
+        private CalculadoraSueldo calculadoraSueldo = new CalculadoraSueldo();
+        public Form1()
             {
                 InitializeComponent();
             }
 
             private void label1_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string apellidos = txtApellidos.Text;
+            double sueldoBruto;
+            if (!double.TryParse(txtSueldoBruto.Text, out sueldoBruto) || sueldoBruto <= 0)
             {
-
+                MessageBox.Show("Ingrese un sueldo bruto válido (mayor que cero).");
+                return;
+            }
+            char categoria = char.ToUpper(txtCategoria.Text[0]);
+            if (categoria != 'A' && categoria != 'B' && categoria != 'C')
+            {
+                MessageBox.Show("Ingrese una categoría válida (A, B o C).");
+                return;
             }
 
-            private void label2_Click(object sender, EventArgs e)
+            Empleado empleado = new Empleado
             {
+                Nombre = nombre,
+                Apellidos = apellidos,
+                SueldoBruto = sueldoBruto,
+                Categoria = categoria
+            };
 
-            }
+            calculadoraSueldo.CalcularAumentoYSueldoNeto(empleado);
+            empleadoDAO.AgregarEmpleado(empleado);
 
-            private void textBox1_TextChanged(object sender, EventArgs e)
+            LimpiarCampos();
+            ActualizarListaEmpleados();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            txtNombre.Clear();
+            txtApellidos.Clear();
+            txtSueldoBruto.Clear();
+            txtCategoria.Clear();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
             {
 
             }
@@ -36,5 +68,32 @@ using System.Windows.Forms;
         {
 
         }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            lstEmpleados.Items.Clear();
+            double totalSueldosNetos = 0;
+
+            foreach (var empleado in empleadoDAO.ObtenerEmpleados())
+            {
+                lstEmpleados.Items.Add($"{empleado.Nombre} {empleado.Apellidos} - {empleado.SueldoBruto:C} - {empleado.MontoAumento:C} - {empleado.SueldoNeto:C}");
+                totalSueldosNetos += empleado.SueldoNeto;
+            }
+
+            lstEmpleados.Items.Add($"Total Sueldos Netos: {totalSueldosNetos:C}");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+    {
+        txtNombre.Clear();
+        txtApellidos.Clear();
+        txtSueldoBruto.Clear();
+        txtCategoria.Clear();
+    }
     }
     }
